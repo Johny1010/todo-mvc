fecac64c4dc7c45b5177a2879dac1e0db6c1adcb
Marcin <mar.nowacki88@gmail.com>
2016-10-04 18:23:59 +0200

    add first todo controller

--- js/app.js
+++ js/app.js
@@ -9,6 +9,41 @@ define([
 
     var myApp = ng.module('app', []);
 
+ var myApp = angular.module('myApp' []);
+
+ myApp.controller('TodoCtrl', ['$scope', function($scope){
+   $scope.todoList = [
+     {
+      name:"todo1",
+      description: "todo1",
+      done: false
+    },
+     {
+        name:"todo2",
+        description: "todo2",
+        done: true
+      },
+       {
+          name:"todo3",
+          description: "todo3",
+          done: false
+        },
+        {
+            name:"todo4",
+            description: "todo4",
+            done: true
+          },
+           {
+              name:"todo5",
+              description: "todo5",
+              done: false
+            }
+   ];
+ }])
+
+
+
+
     /*    myApp.config(function(localStorageServiceProvider){
 
      localStorageServiceProvider
