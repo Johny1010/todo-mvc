0bb53cfc73d60141c8a09b515ae3b97aa5d4d6a5
Robert Beinaravicius <rbeinaravicius@ntrc.eu>
2016-10-04 19:49:01 +0200

    clean project for dev branch

--- js/app.js
+++ js/app.js
@@ -6,37 +6,6 @@ define([
     'use strict';
 
     var myApp = ng.module('app', []);
-
-    myApp.controller('TodoCtrl', ['$scope', function ($scope) {
-
-        $scope.todoList = [
-            {
-                'name': 'todo1',
-                'description': 'todo1',
-                'done': false
-            },
-            {
-                'name': 'todo2',
-                'description': 'todo2',
-                'done': true
-            },
-            {
-                'name': 'todo3',
-                'description': 'todo3',
-                'done': false
-            },
-            {
-                'name': 'todo4',
-                'description': 'todo4',
-                'done': true
-            },
-            {
-                'name': 'todo5',
-                'description': 'todo5',
-                'done': false
-            }
-        ];
-    }]);
-
+    
     return myApp;
 });
