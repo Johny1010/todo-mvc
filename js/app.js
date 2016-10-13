3730c3fd3af40781564548e46c936b8d2ad8fb39
Marcin <mar.nowacki88@gmail.com>
2016-10-13 15:32:35 +0200

    add calculator

--- js/app.js
+++ js/app.js
@@ -6,6 +6,27 @@ define([
     'use strict';
 
     var myApp = ng.module('app', ['ngMaterial']);
+    myApp.controller('calculatorController', function($scope){
+      $scope.result = 0;
+      $scope.add = function(){
+        $scope.result = $scope.a + $scope.b;
+      }
+      $scope.sub = function(){
+        $scope.result = $scope.a - $scope.b;
+      }
+      $scope.mul = function(){
+        $scope.result = $scope.a * $scope.b;
+      }
+      $scope.div = function(){
+        $scope.result = $scope.a / $scope.b;
+      }
+      $scope.sqr = function(){
+        $scope.result = Math.sqrt($scope.a);
+      }
+      $scope.powe = function(){
+        $scope.result = Math.pow($scope.a, $scope.b);
+      }
+    });
 
     return myApp;
 });
