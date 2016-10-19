0063010969622b04fd86b707edd381b0cf5af305
Marcin <mar.nowacki88@gmail.com>
2016-10-19 11:37:40 +0200

    add brutto/netto

--- js/app.js
+++ js/app.js
@@ -6,6 +6,21 @@ define([
     'use strict';
 
     var myApp = ng.module('app', ['ngMaterial']);
+    myApp.controller('PriceCtrl', function($scope){
+      $scope.price = false;
+      $scope.price = 0;
+      $scope.vat = $scope.price * 0.23;
+      $scope.add = function(){
+      $scope.result = $scope.price + $scope.vat;
+      $scope.brutto = $scope.result + ($scope.price*0.23);
+      }
+});
+
+
+
+
+
+
 
     return myApp;
 });
