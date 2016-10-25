1a4555b7875340dd02f9a17b4f189bcad95c362b
Marcin <mar.nowacki88@gmail.com>
2016-10-26 01:31:15 +0200

    new directives

--- js/app.js
+++ js/app.js
@@ -6,70 +6,87 @@ define([
   'use strict';
 
   var myApp = ng.module('app', ['ngMaterial']);
-  myApp.controller('PriceCtrl', function($scope){
-
-    $scope.taxType = 'netto';
-    $scope.price = undefined;
-    $scope.calculate = function(taxType){
-      if (taxType === 'netto') {
-        $scope.result = $scope.price - ($scope.price * 0.23);
-      } else {
-        $scope.result = $scope.price * 1.23;
+  myApp.directive ('priceDirective', function(){
+    return {
+      restrict: 'A',
+      templateUrl: '../templates/price.html',
+      scope: {},
+      controller: function($scope) {
+        $scope.taxType = 'netto';
+        $scope.price = undefined;
+        $scope.calculate = function(taxType){
+          if (taxType === 'netto') {
+            $scope.result = $scope.price - ($scope.price * 0.23);
+          } else {
+            $scope.result = $scope.price * 1.23;
+          }
+        }
       }
     }
 
   });
 
-  myApp.controller('TaxesCtrl', function($scope){
-    $scope.cost = undefined;
-    $scope.calc = function(taxi){
-      if (taxi === '5') {
-        $scope.withVat = $scope.cost * 1.05;
-      } else if (taxi === '8') {
-        $scope.withVat = $scope.cost * 1.08;
-      } else if (taxi === '21') {
-        $scope.withVat = $scope.cost * 1.21;
-      } else {
-        $scope.withVat = $scope.cost * 1.23;
+  myApp.directive('taxesDirective', function(){
+    return {
+      restrict: 'E',
+      templateUrl: '../templates/taxes.html',
+      scope: {},
+      controller: function($scope) {
+        $scope.cost = undefined;
+        $scope.calc = function(){
+          $scope.withVat = $scope.cost * (1 + $scope.taxi/100);
+          $scope.vat = $scope.withVat - $scope.cost;
+        }
       }
-
-      $scope.vat = $scope.withVat - $scope.cost;
     }
   });
 
-  myApp.controller('ExciseCtrl', function($scope){
-    $scope.engine = undefined;
-    $scope.cost = undefined;
-    $scope.calculateExcise = function(){
-      if($scope.engine > 2000) {
-        $scope.withExcise = $scope.cost * 1.186;
-      } else {
-        $scope.withExcise = $scope.cost * 1.031;
+  myApp.directive('exciseDirective', function(){
+    return {
+      restrict: 'C',
+      templateUrl: '../templates/excise.html',
+      scope: {},
+      controller: function($scope) {
+        $scope.engine = undefined;
+        $scope.cost = undefined;
+        $scope.calculateExcise = function(){
+          if($scope.engine > 2000) {
+            $scope.withExcise = $scope.cost * 1.186;
+          } else {
+            $scope.withExcise = $scope.cost * 1.031;
+          }
+          $scope.withoutExcise = $scope.withExcise - $scope.cost;
+        }
       }
-
-      $scope.withoutExcise = $scope.withExcise - $scope.cost;
     }
   });
 
-  myApp.controller('InvestmentCtrl', function($scope){
-    $scope.cost = undefined;
-    $scope.procent = undefined;
-    $scope.months = 'one';
-    $scope.invest = function(){
-      var initInterest = $scope.cost/100 * $scope.procent;
+  myApp.directive('investmentDirective', function(){
+    return {
+      restrict: 'C',
+      templateUrl: '../templates/investment.html',
+      scope: {},
+      controller: function($scope) {
+        $scope.cost = undefined;
+        $scope.procent = undefined;
+        $scope.months = 'one';
+        $scope.invest = function(){
+          var initInterest = $scope.cost/100 * $scope.percentage;
 
-      if ($scope.months === 'one'){
-        $scope.amount = ($scope.cost + (initInterest * 0.08333));
-        $scope.interest = initInterest * 0.08333;
-      } else if ($scope.months === 'three'){
-        $scope.amount = ($scope.cost + (initInterest * 0.25));
-        $scope.interest = initInterest * 0.25;
-      } else if ($scope.months === 'six'){
-        $scope.amount = ($scope.cost + (initInterest * 0.5));
-        $scope.interest = initInterest * 0.5;
-      } else {
-        $scope.amount = $scope.cost + initInterest;
-        $scope.interest = initInterest;
+          if ($scope.months === 'one'){
+            $scope.amount = ($scope.cost + (initInterest * 0.08333));
+            $scope.interest = initInterest * 0.08333;
+          } else if ($scope.months === 'three'){
+            $scope.amount = ($scope.cost + (initInterest * 0.25));
+            $scope.interest = initInterest * 0.25;
+          } else if ($scope.months === 'six'){
+            $scope.amount = ($scope.cost + (initInterest * 0.5));
+            $scope.interest = initInterest * 0.5;
+          } else {
+            $scope.amount = $scope.cost + initInterest;
+            $scope.interest = initInterest;
+          }
+        }
       }
     }
   });
